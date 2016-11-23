00a70e607ab9e46bc1a84888eee6364299e023c3
Sean Coates <sean.coates+fooyay@gmail.com>
2016-11-23 18:12:59 -0500

    Create Business model and change BusinessesController to use it.

--- app/Http/Controllers/BusinessesController.php
+++ app/Http/Controllers/BusinessesController.php
@@ -2,13 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Business;
 use Illuminate\Http\Request;
 
 class BusinessesController extends Controller
 {
     public function index()
     {
-        $businesses = \DB::table('businesses')->get();
+        $businesses = Business::all();
 
         return view('businesses.index', compact('businesses'));
     }
